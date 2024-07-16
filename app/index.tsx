467ad6d3fdce560605b6677e92663b8f94acec07
00156257 Shakeer Mohammed Audhil <00156257shakeermohammedaudhil@Audhil00156257s-MacBook-Pro.local>
2024-07-16 12:30:57 +0800

    add nativewind/tailwind for css styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,15 @@
 //  home page / of the website
 //  this layout will be available in all screens of the app, like header, footer
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Link } from 'expo-router'
 
 const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Index.jsx Mohammed Audhil Jadfdck and jill</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Index.jsx Mohammed Audhil checking
+      </Text>
       <StatusBar style='auto'/>
       <Link href="/profile" style={{color: 'red'}}>Go to profile</Link>
     </View>
@@ -17,12 +18,3 @@ const RootLayout = () => {
 }
 
 export default RootLayout
-
-const styles = StyleSheet.create({
-  container:{
-    display: 'flex',
-    alignItems:'center',
-    justifyContent: 'center',
-    flex: 1
-  }
-})
--- app/profile.tsx
+++ app/profile.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const profile = () => {
   return (
     <View>
-      <Text>profile</Text>
+      <Text>Jack and jill went up to hill!</Text>
     </View>
   )
 }
