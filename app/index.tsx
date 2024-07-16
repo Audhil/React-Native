b6858b5609823c7ce94e09bc1f287789e81eac81
00156257 Shakeer Mohammed Audhil <00156257shakeermohammedaudhil@Audhil06257sMBP.dlinkrouter.local>
2024-07-16 17:22:57 +0800

    add ScrollView

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,36 @@
 //  home page / of the website
 //  this layout will be available in all screens of the app, like header, footer
-import { Text, View } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { Link } from 'expo-router'
+import React from 'react';
+import { Image, ScrollView, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
 
 const RootLayout = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl font-pblack'>Aora!
-      </Text>
-      <StatusBar style='auto'/>
-      <Link href="/home" style={{color: 'red'}}>Go to Home</Link>
-    </View>
-  )
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+        </View>
 
-}
+        <View className="relative mt-5">
+          <Text className="text-3xl text-white font-bold text-center">
+            Jack and jill went{'\n'} up the hill to fetch pail of water! with{' '}
+            <Text className="text-secondary-200">Aora!</Text>
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
