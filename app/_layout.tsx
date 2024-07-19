b0b0f28d35ce4c579c1ccacf6ed081bb98413a8f
00156257 Shakeer Mohammed Audhil <00156257shakeermohammedaudhil@Audhil00156257s-MacBook-Pro.local>
2024-07-19 11:12:58 +0800

    add sign-in screen with logics

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 //  this layout will be available in all screens of the app, like header, footer
-import React, { useEffect } from 'react'
-import { SplashScreen, Stack } from 'expo-router'
+import React, { useEffect } from 'react';
+import { SplashScreen, Stack } from 'expo-router';
 import { useFonts } from 'expo-font';
 
 // const RootLayout = () => {
@@ -40,32 +40,35 @@ import { useFonts } from 'expo-font';
 //  3. to use stack - alternative approachl than using Slot
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
-    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
-    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
-    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
-    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
-    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
-    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
-    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-ExtraBold': require('../assets/fonts/Poppins-ExtraBold.ttf'),
+    'Poppins-ExtraLight': require('../assets/fonts/Poppins-ExtraLight.ttf'),
+    'Poppins-Light': require('../assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Thin': require('../assets/fonts/Poppins-Thin.ttf'),
   });
 
   useEffect(() => {
-    if(error) throw error;
-    if(fontsLoaded) SplashScreen.hideAsync();
-  },[fontsLoaded, error]);
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
 
-  if(!fontsLoaded && !error) return null;
+  if (!fontsLoaded && !error) return null;
 
   return (
-  <Stack>
-    <Stack.Screen name="index" options={{headerShown: false}} />
-  </Stack>
-  )
-}
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
+    </Stack>
+  );
+};
 
 //  it'll prevent splash screen to get hide before assets are loaded
 SplashScreen.preventAutoHideAsync();
 
-export default RootLayout
+export default RootLayout;
